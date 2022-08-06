102d46d1cfca8350da5b3fdfcbd7df0acff3f6f7
Nishit <nishitsurti312@gmail.com>
2022-08-06 21:13:18 +0530

    HTML, CSS AND JAVASCRIPT Projects

---
+++ COMPLETE WEB DEVELOPMENT BOOTCAMP/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.component {
+  render() {
+    return (
+      <div>
+        <p>Hello This is my first react app</p>
+      </div>
+    );
+  }
+}
+
+export default App;
